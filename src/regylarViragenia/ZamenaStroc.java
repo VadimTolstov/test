bafa1c79edd68f5b28efca4e23d231339bdf3de0
VAdimT <vadimt382@gmail.com>
2022-11-27 17:15:00 +0300

    S1

--- src/regylarViragenia/ZamenaStroc.java
+++ src/regylarViragenia/ZamenaStroc.java
@@ -7,7 +7,6 @@ public class ZamenaStroc {
     public static void main(String[] args) {
         String text = "Егор Алла Анна";
         Pattern pattern = Pattern.compile("А.+?а");
-
         Matcher matcher = pattern.matcher(text);
         while (matcher.find()) {
             int start=matcher.start();
