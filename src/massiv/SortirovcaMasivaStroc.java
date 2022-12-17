0b40d79dfd0d05c115a787bfeb94305313c90011
VAdimT <vadimt382@gmail.com>
2022-12-17 17:06:54 +0300

    S6

---
+++ src/massiv/SortirovcaMasivaStroc.java
@@ -0,0 +1,27 @@
+package massiv;
+import java.util.Arrays;
+import java.util.Comparator;
+
+class ComparatorByLength implements Comparator<String> {
+
+    @Override
+    public int compare(String arg0, String arg1) {
+        if (arg0 == null)
+            return 1;
+        if (arg1 == null)
+            return -1;
+        return arg0.length() - arg1.length();
+    }
+}
+
+
+class Main {
+
+    public static void main(String[] args) {
+        String[] array = {"Баркас","Лес", "Арбуз", "Звезда", "Лимон", "Дерево","Ёлка"};
+         Arrays.sort(array, new ComparatorByLength());
+        System.out.println(Arrays.toString(array));
+        Arrays.sort(array);
+        System.out.println(Arrays.toString(array));
+    }
+}//Ё это проблемма сортировки испльзуй лудше е
