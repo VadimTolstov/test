20f0b4ae601e61aaad4c439bfaa6762b28bd8c3e
VAdimT <vadimt382@gmail.com>
2023-01-17 18:39:41 +0300

    Тест

--- src/While.java
+++
@@ -1,9 +0,0 @@
-public class While {
-    public static void main(String[] args) {
-        int a = 0;
-        while (a <= 100) {
-            System.out.println(++a);
-        }
-    }
-}
-// Выполняет цикл пока не выполнит условие
