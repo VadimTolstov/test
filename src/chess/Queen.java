2f5590717e4219545e704334ed7f3f6f79ede625
VAdimT <vadimt382@gmail.com>
2023-01-19 17:24:04 +0300

    Шахматы2

--- src/chess/Queen.java
+++ src/chess/Queen.java
@@ -1,13 +1,13 @@
 package chess;
 
-public class    Bishop extends ChessPiece {
-    public Bishop(String color) {
+public class Queen extends ChessPiece {
+    public Queen(String color) {
         super(color);
     }
 
     @Override
     public String getColor() {
-        return color;
+        return this.color;
     }
 
     @Override
@@ -15,13 +15,13 @@ public class    Bishop extends ChessPiece {
 
         if (line != toLine && column != toColumn &&
             getMax(line, toLine) - getMin(line, toLine) == getMax(column, toColumn) - getMin(column, toColumn) &&
-            checkPos(line) && checkPos(column) && checkPos(toLine) && checkPos(toColumn) &&
+            checkPos(line) && checkPos(column) && checkPos(toLine) &&
+            checkPos(toColumn) &&
             (chessBoard.board[toLine][toColumn] == null || !chessBoard.board[toLine][toColumn].color.equals(this.color)) &&
             chessBoard.board[line][column] != null) {
             if (!chessBoard.board[line][column].equals(this)) {
                 return false;
             }
-
 
             if ((column == getMin(column, toColumn) && line == getMax(line, toLine)) ||
                 (toColumn == getMin(column, toColumn) && toLine == getMax(line, toLine))) {
@@ -58,12 +58,51 @@ public class    Bishop extends ChessPiece {
                 }
                 return true;
             }
+        } else if (checkPos(line) && checkPos(column) && checkPos(toLine) && checkPos(toColumn)) {
+
+            if (column == toColumn) {
+
+                for (int i = getMin(line, toLine); i < getMax(line, toLine); i++) {
+                    if (chessBoard.board[i][column] != null) {
+                        if (chessBoard.board[i][column] == this && i == getMax(line, toLine)) return false;
+                        else if (chessBoard.board[i][column].getColor().equals(this.color) && i == toLine)
+                            return false;
+                        else if (!chessBoard.board[i][column].getColor().equals(this.color) && i == toLine)
+                            return true;
+                        else if (i != toLine && i != line) return false;
+                    }
+                }
+
+                if (chessBoard.board[toLine][column] != null) {
+                    if (chessBoard.board[toLine][column].getColor().equals(this.color) && chessBoard.board[toLine][column] != this)
+                        return false;
+                    else return !chessBoard.board[toLine][column].getColor().equals(this.color) && chessBoard.board[toLine][column] != this;
+                } else return true;
+            } else if (line == toLine) {
+
+                for (int i = getMin(toColumn, column); i < getMax(column, toColumn); i++) {
+                    if (chessBoard.board[line][i] != null) {
+                        if (chessBoard.board[line][i] == this && i == getMax(column, toColumn)) return false;
+                        else if (chessBoard.board[line][i].getColor().equals(this.color) && i == toColumn)
+                            return false;
+                        else if (!chessBoard.board[line][i].getColor().equals(this.color) && i == toColumn)
+                            return true;
+                        else if (i != toLine && i != column) return false;
+                    }
+                }
+
+                if (chessBoard.board[toLine][toColumn] != null) {
+                    if (chessBoard.board[toLine][toColumn].getColor().equals(this.color) && chessBoard.board[toLine][toColumn] != this)
+                        return false;
+                    else return !chessBoard.board[toLine][toColumn].getColor().equals(this.color) && chessBoard.board[toLine][toColumn] != this;
+                } else return true;
+            } else return false;
         } else return false;
     }
 
     @Override
     public String getSymbol() {
-        return "B";
+        return "Q";
     }
 
     public int getMax(int a, int b) {
